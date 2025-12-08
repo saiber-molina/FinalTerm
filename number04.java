d106d119deed4c8e4de2c03323c6850379e3496c
saiber-molina <saibermolina028@gmail.com>
2025-12-09 02:17:09 +0800

    number04.java

--- number04.java
+++ number04.java
@@ -9,19 +9,15 @@ import javax.swing.*;
 
 public class number4 {
     public static void main(String[] args) {
-        int[] num = new int[3];
-
-        for (int i = 0; i < 3; i++) {
-            num[i] = Integer.parseInt(JOptionPane.showInputDialog("Enter number #" + (i+1) + ":"));
-        }
+        int[] num = {11,22,21};
 
         int smallest = num[0];
         int largest = num[0];
 
-        for (int i = 1; i < 3; i++) {
-            if (num[i] < smallest);
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] < smallest)
             smallest = num[i];
-            if (num[i] > largest);
+            if (num[i] > largest)
             largest = num[i];
         }
 
